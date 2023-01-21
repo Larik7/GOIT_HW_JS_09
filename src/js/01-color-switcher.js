bfa0e9981623a72ee845626f1cbe086c3564b455
Larik7 <Larik777taganka@gmail.com>
2023-01-21 17:12:17 +0200

    second commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyBgColor() {
+  document.body.style.backgroundColor = getRandomHexColor();  
+}
+
+function switchButtonsDisabled() {
+  btnStart.toggleAttribute('disabled');
+  btnStop.toggleAttribute('disabled');
+}
+
+startBtn.addEventListener("click", () => {     
+    timerId = setInterval(changeBodyBgColor, 1000);
+    switchButtonsDisabled();
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  switchButtonsDisabled();
+});
